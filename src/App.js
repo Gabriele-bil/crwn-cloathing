fa32007d487c69ef3469ef16dc5d219f6d42d524
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-05 17:03:53 +0100

    fine lez_80

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
