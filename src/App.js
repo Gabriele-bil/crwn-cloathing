810dd584e18607315cf452896e9cbaff90ed7685
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-05 21:51:05 +0100

    fine lez_83_84_85

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { HomePage } from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+
 import "./App.css";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
       {/* <HomePage /> */}
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
