6c6f58da141cc4b909b1ab609278d16bde00a735
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-05 22:56:15 +0100

    fine lez_89_90_91

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ShopPage from "./pages/shop/shop.component";
 import HeaderComponent from "./components/header/header.component";
 
 import "./App.css";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
