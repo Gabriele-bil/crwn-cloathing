3f8e1ae78801052aff719729a97a0546040265aa
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-10 19:57:34 +0100

    fine lez_127_128_129

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartActionsType from "./cart.type";
 export const toggleCartHidden = () => ({
   type: CartActionsType.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: CartActionsType.ADD_ITEM,
+  payload: item,
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,20 @@
 import CartActionsType from "./cart.type";
+import { addItemToCart } from "./cart.utility";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionsType.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+    case CartActionsType.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
