d7ce20cb2ef9370f17d4090c9f1fd4d045bd4217
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-14 22:31:56 +0100

    fine lez_183_184

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -35,6 +35,23 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollections = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollections.reduce((accumalator, collection) => {
+    accumalator[collection.title.toLowerCase()] = collection;
+    return accumalator;
+  }, {});
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
