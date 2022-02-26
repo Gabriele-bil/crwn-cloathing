6894ed83a94fa2cdeb405a5596453a2a552f6de0
GabrieleBi <gabriele.bilello1@gmail.com>
2022-02-26 12:12:48 +0100

    fine lez_228

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import CollectionItem from "../../components/collection-item/collection-item.component";
-
+import { useParams } from "react-router-dom";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import {
@@ -10,8 +10,12 @@ import {
   CollectionItemsContainer,
 } from "./collection.styles";
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = () => {
+  const { collectionId } = useParams();
+  const collection = useSelector(selectCollection(collectionId));
+
   const { title, items } = collection;
+
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
@@ -24,8 +28,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
